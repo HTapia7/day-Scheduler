3a6fc5e5eae9cca303433725eb6fd4493fabff62
Hernan Tapia <soliditize@MacBook-Pro.local>
2022-10-20 14:13:54 -0700

    Added on click for btns and color coded time slots

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,24 +1,45 @@
 // Var for todays time 
 var today = moment();
 
+// Show the clock in header 
 $('#currentDay').text(today.format('MMMM Do YYYY, h:mm:ss a'));
 
- var timeOfDay = $('.hour');
- var userInput = $('.description');
- var saveBtn = $('.fa-save')
 
+ // Grabs the values from textbox when clicked 
+$(document).on('click', '.saveBtn' , saveNote )
 
+ function saveNote(){
+   console.log(this)
 
- saveBtn.on('click', function() {  
-    
-    var user = userInput.val();
+   // save items to local storage
+   var time = this.value
+   var note = $('#textArea-' + time).val()
+   localStorage.setItem(time , note )
+   var persistData = localStorage.getItem(time , note)
+   console.log(persistData)
+   
+  
+ }
 
-    console.log(user)
-    
-    localStorage.setItem('9AM' , user)
-    // localStorage.setItem('9AM' , JSON.stringify(user))
+function stylingTime(){ 
+
+  var currentTime = moment().hour();
+
+// split the ID so I can get the hour of time 
+ $('.time-block').each(function(){
+  var workTime = parseInt($(this).attr('id').split('hour')[1])
+
+  // Loop to add class depending on the hour 
+  if (workTime < currentTime) {
+    $(this).addClass('past');
+    }
+  else if (workTime == currentTime) {  
+    $(this).addClass('present');
+    }
+  else {
+    $(this).addClass('future');
+    }
  })
+}
 
-
-
- console.log(this)
+stylingTime()
